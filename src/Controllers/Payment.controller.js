48656b55082a4f3c431cc75a7587f7650159d7bd
Keyur Pathar <keyurpathar005@gmail.com>
2026-04-24 11:10:24 +0530

    be repaired

--- src/Controllers/Payment.controller.js
+++ src/Controllers/Payment.controller.js
@@ -1,26 +1,23 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripeLib = require("stripe");
 
 const createPayment = async (req, res) => {
     try {
+        const stripe = stripeLib(process.env.STRIPE_SECRET_KEY);
         const { name, price } = req.body;
 
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ["card"],
             mode: "payment",
-
             line_items: [{
                 price_data: {
                     currency: "inr",
-                    product_data: {
-                        name: name,
-                    },
-                    unit_amount: price * 100, 
+                    product_data: { name },
+                    unit_amount: price * 100,
                 },
                 quantity: 1,
             }],
-
-            success_url: "http://localhost:5173/success",
-            cancel_url: "http://localhost:5173/cancel",
+            success_url: `${process.env.FRONTEND_URL}/success`,
+            cancel_url: `${process.env.FRONTEND_URL}/cancel`,
         });
 
         res.json({ url: session.url });
@@ -30,8 +27,8 @@ const createPayment = async (req, res) => {
             success: false,
             message: err.message,
             data: null
-        })
+        });
     }
-}
+};
 
-module.exports = { createPayment }
+module.exports = { createPayment };

a085cc07a0e5399980def7004c789facf609b88b
Keyur Pathar <keyurpathar005@gmail.com>
2026-04-24 10:58:22 +0530

    backend ready for deployment

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./DB/db");
+const port = process.env.PORT || 5000
 
 // db connection 
 connectDB()
 
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
     console.log("server started at http://localhost:5000")
 })  
